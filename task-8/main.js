96915f7afad7ba14eeae2fb26cd75372912e7332
Sarkhan <syako44@gmail.com>
2022-06-30 00:55:25 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -12,15 +12,20 @@ prompt.get('input', function (err, result) {
     var input_data = input.split(','); // ['5', '2 4 7 12 9']
     var n = parseInt(input_data[0]); // Massivin element sayı. Nümunə - 5
     var arr = input_data[1].split(' '); // Daxil edilmiş massiv. Nümunə - ['2', '4', '7', '12', '9']
-    let end = arr[arr.length - 1]
-let max = 0
+    let max = 0 // 7
+let end = arr[arr.length - 1] // -5
+let index;
 
-for(i = 0 ; i < arr.length ; i++){
+for(i = arr.length - 1 ; i >= 0 ; i--){
     if(arr[i] > max){
         max = arr[i]
+        index = i
     }
 }
-arr[arr.indexOf(max, arr.indexOf(max) + 1)] = end
-arr[arr.length - 1] = max
+
+let endindex = arr.indexOf(end)
+arr[index] = end
+arr[endindex] = max
+
 console.log(arr);
 });
